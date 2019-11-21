14891be3ead4433e3aad965f2c2ea42baee27e3a
Artur Siembab <ajtuss@gmail.com>
2019-11-21 18:28:34 +0100

    Generate with spring initializr

---
+++ src/test/java/pl/ajtuss/cinematics/CinematicsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ajtuss.cinematics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
