05edd4de05e6dddacb30a64391278558c977916c
Artur Siembab <ajtuss@gmail.com>
2019-11-23 09:12:09 +0100

    Add model

---
+++ src/main/java/pl/ajtuss/cinematics/model/Image.java
@@ -0,0 +1,16 @@
+package pl.ajtuss.cinematics.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+  private String url;
+
+  private String filename;
+
+}
---
+++ src/main/java/pl/ajtuss/cinematics/model/Movie.java
@@ -0,0 +1,29 @@
+package pl.ajtuss.cinematics.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.With;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@Document
+public class Movie {
+
+  @Id
+  @JsonProperty(access = Access.READ_ONLY)
+  private String id;
+
+  private String name;
+
+  private String description;
+
+  @With
+  @JsonProperty(access = Access.READ_ONLY)
+  private Image image;
+
+}
+
