03d59e3ed51c0dcf0d83ce1cb598135602dd3b14
asiembab <siembab.artur@gmail.com>
2019-11-24 18:03:25 +0100

    Add noArgs constructor

--- src/main/java/pl/ajtuss/cinematics/model/Movie.java
+++ src/main/java/pl/ajtuss/cinematics/model/Movie.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.With;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document
 public class Movie {
 
