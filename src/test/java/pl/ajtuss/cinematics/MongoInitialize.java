f520af286ade395ee7d28398b96750f2ff5b5415
asiembab <siembab.artur@gmail.com>
2019-11-25 19:15:35 +0100

    Add initialize mongo abstract test

---
+++ src/test/java/pl/ajtuss/cinematics/MongoInitialize.java
@@ -0,0 +1,37 @@
+package pl.ajtuss.cinematics;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.util.ResourceUtils;
+
+public abstract class MongoInitialize {
+
+  private static String MOVIE = "movie";
+
+  @Autowired
+  MongoTemplate mongoTemplate;
+
+  private static final JacksonJsonParser PARSER = new JacksonJsonParser();
+
+  @BeforeEach
+  protected void initializeDb() throws IOException {
+    File file = ResourceUtils.getFile("classpath:movies.json");
+    byte[] bytes = Files.readAllBytes(file.toPath());
+    String json = new String(bytes);
+    List<Object> objects = PARSER.parseList(json);
+    objects.forEach(i -> mongoTemplate.save(i, MOVIE));
+  }
+
+  @AfterEach
+  protected void cleanDb() {
+    mongoTemplate.dropCollection(MOVIE);
+  }
+
+}
