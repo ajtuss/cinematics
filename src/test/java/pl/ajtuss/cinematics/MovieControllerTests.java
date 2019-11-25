0eb5eb1897d7eaa0c8f89368a6192536bcd29b32
asiembab <siembab.artur@gmail.com>
2019-11-25 19:16:38 +0100

    Update tests

--- src/test/java/pl/ajtuss/cinematics/MovieControllerTests.java
+++ src/test/java/pl/ajtuss/cinematics/MovieControllerTests.java
@@ -1,5 +1,7 @@
 package pl.ajtuss.cinematics;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -7,9 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,11 +22,12 @@ import org.springframework.web.context.WebApplicationContext;
 import pl.ajtuss.cinematics.model.Movie;
 
 @SpringBootTest
-public class MovieControllerTests {
+public class MovieControllerTests extends MongoInitialize {
+
+  private static JsonMapper mapper = new JsonMapper();
+  private static String API_URL = "/api/movies/";
 
   private MockMvc mockMvc;
-
-  private JsonMapper mapper = new JsonMapper();
 
   @BeforeEach
   void setup(WebApplicationContext wac) {
@@ -30,11 +36,28 @@ public class MovieControllerTests {
 
   @Test
   void getMovies() throws Exception {
-    this.mockMvc.perform(get("/api/movies")
+    this.mockMvc.perform(get(API_URL)
         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.content").isArray());
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content", hasSize(20)));
+  }
+
+  @Test
+  void getMovie() throws Exception {
+    Movie anyMovie = mongoTemplate.findOne(Query.query(new Criteria()), Movie.class);
+    Assertions.assertThat(anyMovie).isNotNull();
+
+    this.mockMvc.perform(get(API_URL + anyMovie.getId())
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.id").value(anyMovie.getId()))
+                .andExpect(jsonPath("$.name").value(anyMovie.getName()))
+                .andExpect(jsonPath("$.description").value(anyMovie.getDescription()))
+                .andExpect(jsonPath("$.image.filename").value(anyMovie.getImage().getFilename()))
+                .andExpect(jsonPath("$.image.url").value(anyMovie.getImage().getUrl()));
   }
 
   @Test
@@ -44,7 +67,7 @@ public class MovieControllerTests {
     movie.setDescription("Description");
     String movieJson = mapper.writeValueAsString(movie);
 
-    this.mockMvc.perform(post("/api/movies")
+    this.mockMvc.perform(post(API_URL)
         .accept(MediaType.APPLICATION_JSON)
         .content(movieJson)
         .contentType(MediaType.APPLICATION_JSON))
@@ -58,5 +81,34 @@ public class MovieControllerTests {
   @Test
   void deleteMovie() throws Exception {
 
+    Movie anyMovie = mongoTemplate.findOne(Query.query(new Criteria()), Movie.class);
+    Assertions.assertThat(anyMovie).isNotNull();
+
+    this.mockMvc.perform(delete(API_URL + anyMovie.getId())
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+    Movie shouldBeNull = mongoTemplate.findById(anyMovie.getId(), Movie.class);
+    Assertions.assertThat(shouldBeNull).isNull();
+  }
+
+  @Test
+  void searchMovieContainsInTitle() throws Exception {
+
+    Movie aMovie = mongoTemplate.findOne(Query.query(Criteria.where("name").is("Civilization")), Movie.class);
+    Assertions.assertThat(aMovie).isNotNull();
+
+    this.mockMvc.perform(get(API_URL)
+        .param("s", "vilizat")
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content.[0].id").value(aMovie.getId()))
+                .andExpect(jsonPath("$.content.[0].name").value(aMovie.getName()))
+                .andExpect(jsonPath("$.content.[0].description").value(aMovie.getDescription()))
+                .andExpect(jsonPath("$.content.[0].image.filename").value(aMovie.getImage().getFilename()))
+                .andExpect(jsonPath("$.content.[0].image.url").value(aMovie.getImage().getUrl()));
   }
 }
--- src/test/java/pl/ajtuss/cinematics/MovieRepositoryTests.java
+++ src/test/java/pl/ajtuss/cinematics/MovieRepositoryTests.java
@@ -5,19 +5,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import pl.ajtuss.cinematics.model.Movie;
 import pl.ajtuss.cinematics.repository.MovieRepository;
 
 @DataMongoTest
-public class MovieRepositoryTests {
-
+public class MovieRepositoryTests extends MongoInitialize {
 
   @Autowired
   private MovieRepository movieRepository;
 
   @Test
-  public void testSaveAndFind() {
-
+  void saveShouldPersistObject() {
     Movie movie = new Movie();
     movie.setName("Title");
     movie.setDescription("Description");
@@ -26,6 +26,17 @@ public class MovieRepositoryTests {
     Optional<Movie> byId = movieRepository.findById(saved.getId());
     Assertions.assertThat(byId.isPresent()).isTrue();
     Assertions.assertThat(byId.get()).isEqualTo(saved);
+  }
+
+  @Test
+  void findAllByNameContainsShouldFind() {
+    String searched = "vilizati";
+    PageRequest pageable = PageRequest.of(0, 100);
+
+    Page<Movie> found = movieRepository.findAllByNameContains(searched, pageable);
+
+    Assertions.assertThat(found.getTotalElements()).isEqualTo(1);
+    Assertions.assertThat(found).allMatch(m -> m.getName().contains(searched));
   }
 
 
